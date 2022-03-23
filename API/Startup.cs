820cdfa7fc80b7b5b0d4d35d104104e8d5eca466
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2022-03-23 10:46:43 +0200

    chore: add database configurations

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Persistence;
 
 namespace API
 {
@@ -22,6 +24,24 @@ namespace API
         }
 
         public IConfiguration Configuration { get; }
+
+        // Database configuration
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("PostGreSql"));
+            });
+            ConfigureServices(services);
+        }  public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("PostGreSql"));
+            });
+            ConfigureServices(services);
+        }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
