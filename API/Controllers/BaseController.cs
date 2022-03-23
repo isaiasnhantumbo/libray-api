d6f5bd13dc038a62850ad879864d66b4446309c7
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2022-03-23 15:52:48 +0200

    chore: add base controller

---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : Controller
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
