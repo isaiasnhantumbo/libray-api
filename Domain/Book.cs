b60588580c7efef2f40efa95ba341667c53b3f3e
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2022-03-23 10:06:58 +0200

    feat: add project entities

---
+++ Domain/Book.cs
@@ -0,0 +1,10 @@
+namespace Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+}
